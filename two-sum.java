59f6651b905d2c3ccdd2c6bd4ef4ff2b3ddd847f
Vi Nguyen <nguyenvv13@gmail.com>
2025-05-22 03:08:21 -0400

    Add an alternative algorithm to reduce time order complexity

--- two-sum.java
+++ two-sum.java
@@ -1,9 +1,12 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 class Solution {
     static int[] twoSum(int[] nums, int target) {
         int[] indices = new int[2];
 
+        // Method 1: O(n^2)
+        /*
         for(int i = 0; i < nums.length - 1; i++) {
             for(int j = i + 1; j < nums.length; j++) {
                 if(nums[i] + nums[j] == target) {
@@ -13,12 +16,31 @@ class Solution {
                 }
             }
         }
+        */
+
+        // Method 2: O(n)
+        HashMap<Integer, Integer> diff_mapping = new HashMap<>(nums.length);
+
+        for(int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+            Integer diff_index = diff_mapping.get(num);
+            
+            if(diff_index != null) {
+                indices[0] = diff_index;
+                indices[1] = i;
+                break;
+            }
+            else {
+                diff_mapping.put(diff, i);
+            }
+        }
 
         return indices;
     }
 
     public static void main(String[] args) {
-        int[] nums = {0,4,3,0};
-        System.out.println(Arrays.toString(twoSum(nums, 0)));
+        int[] nums = {2,7,11,15};
+        System.out.println(Arrays.toString(twoSum(nums, 9)));
     }
 }
