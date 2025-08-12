de00fa3e6bccea6865aad8f07bfb9984a93cec6a
Vi Nguyen <nguyenvv13@gmail.com>
2025-08-12 03:48:01 -0400

    word reversal with space preservation

---
+++ reverse-words/reverse-words.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(ReverseWords("double  spaces"))
+}
+
+func ReverseWords(str string) string {
+	// Tokenize the sentence
+	var words []string = strings.Split(str, " ")
+	var reverses = []string{}
+
+	// Get each word
+	for _, word := range words {
+		var reverse string = ""
+
+		// Get each character and make a new word that's reversed
+		for _, char := range word {
+			reverse = string(char) + reverse
+		}
+
+		reverses = append(reverses, string(reverse))
+	}
+
+	return strings.Join(reverses, " ") // reverse those words
+}
